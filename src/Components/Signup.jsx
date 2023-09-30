682cf1d2d0b4787e99485a0373ad1879a6009fac
rashidsarkar <rashidrock558@gmail.com>
2023-09-30 17:25:07 +0600

    icon add for hide and show

--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -1,35 +1,36 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import auth from "../firebase/firebase.config";
 import { useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa"; // Import react-icons
 
 function Signup() {
   const [error, setError] = useState("");
   const [success, setSuccess] = useState("");
   const [showPass, setShowPass] = useState(false);
 
-  const handleSingUp = (e) => {
+  const handleSignUp = (e) => {
     setError("");
     setSuccess("");
     e.preventDefault();
 
     const email = e.target.email.value;
     const pass = e.target.password.value;
-    console.log(typeof pass);
+
     if (pass.length < 6) {
-      setError("password should be 6 cereckter or longer");
+      setError("Password should be 6 characters or longer");
       return;
     } else if (!/[A-Z]/.test(pass)) {
-      setError("your pass have to Upper case");
+      setError("Your password must contain at least one uppercase letter");
       return;
     }
 
-    // create user
     createUserWithEmailAndPassword(auth, email, pass)
-      .then((res) => setSuccess("User Creat Done"))
+      .then((res) => setSuccess("User Created Successfully"))
       .catch((err) => {
         setError(err.message);
       });
   };
+
   return (
     <div>
       <div className="flex items-center justify-center min-h-screen bg-gray-100">
@@ -37,7 +38,7 @@ function Signup() {
           <h2 className="mb-4 text-2xl font-semibold text-gray-800">Signup</h2>
           {error && <p className="text-red-500">{error}</p>}
           {success && <p className="text-green-500">{success}</p>}
-          <form onSubmit={handleSingUp}>
+          <form onSubmit={handleSignUp}>
             <div className="mb-4">
               <label
                 className="block mb-1 text-sm font-medium text-gray-600"
@@ -53,26 +54,33 @@ function Signup() {
                 required
               />
             </div>
-            <div className="mb-4">
-              <label
-                className="block mb-1 text-sm font-medium text-gray-600"
-                htmlFor="password"
-              >
-                Password
-              </label>
+            <label
+              className="block mb-1 text-sm font-medium text-gray-600"
+              htmlFor="password"
+            >
+              Password
+            </label>
+            {/* Password input with eye icons */}
+            <div className="relative flex items-center mb-4">
               <input
                 className="w-full px-3 py-2 border rounded-lg outline-none focus:border-blue-500"
-                type={showPass ? "password" : "text"}
+                type={showPass ? "text" : "password"}
                 id="password"
                 required
                 name="password"
               />
-              <span
-                onClick={() => setShowPass(!showPass)}
-                className="bg-slate-500"
-              >
-                Show Pass
-              </span>
+              {/* Show Password icon */}
+              {showPass ? (
+                <FaEye
+                  className="absolute w-6 h-6 text-gray-500 cursor-pointer right-3 top-2"
+                  onClick={() => setShowPass(!showPass)}
+                />
+              ) : (
+                <FaEyeSlash
+                  className="absolute w-6 h-6 text-gray-500 cursor-pointer right-3 top-2"
+                  onClick={() => setShowPass(!showPass)}
+                />
+              )}
             </div>
             <input
               className="w-full py-2 text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
